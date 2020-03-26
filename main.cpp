641ff5a30b9ce2971e39019d579a8fae30fb7440
Alessio <alessiogdanz@gmail.com>
2020-03-26 16:53:16 -0500

    Added prototype to main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,50 @@
 #include <vector>
 using namespace std;
 
+void chasing(int **A[], int a, int *B[], int b, int C[], int c);
+
+int main() {
+        int a = 5, b = 4, c = 3;
+        // First you need to initialize all elements in A and B to NULL
+        int **A[5] = {}, *B[4] = {}, C[3] = {2, 1, 4};
+        // A is an array of pointers, and each non-NULL element points to an element of B
+        // B is an array of pointers, and each non-NULL element points to an element of C
+        chasing(A, a, B, b, C, c);
+
+        int **ansA[5] = {} ,*ansB[4] = {};
+        for (int i = 0; i < a; i++)
+                ansA[i] = NULL;
+        for (int i = 0; i < b; i++)
+                ansB[i] = NULL;
+        
+        char sampleInputNum = 3; // Change number depending on the sample inputs
+        switch (sampleInputNum)
+        {
+        case 1:
+                ansA[0] = &B[0], ansA[1] = &B[2], ansB[0] = &C[2], ansB[1] = &C[1];
+                break;
+        case 2:
+                ansA[0] = &B[1];
+                break;
+        case 3:
+                ansA[2] = &B[1], ansA[3] = &B[0], ansB[1] = &C[1], ansB[2] = &C[0];
+                break;
+        default:
+                break;
+        }
+        // Check A
+        for (int i = 0; i < a; i++)
+                if (A[i] != ansA[i] )
+                cout << "A[" << i << "] " << "wrong answer" << endl;
+ 
+        // Check B
+        for (int i = 0; i < b; i++)
+                if (B[i] != ansB[i] )
+                        cout << "B[" << i << "] " << "wrong answer" << endl;
+
+        return 0;
+}
+
 void chasing(int **A[], int a, int *B[], int b, int C[], int c){
         // Another way to input data, but if a wrong string is inputed, error is thrown
         /*scanf(" %c %d  %c %d", &letter1, &n, &letter2, &m); // the %c conversion specifier won't automatically skip any leading whitespace
@@ -65,49 +109,6 @@ void chasing(int **A[], int a, int *B[], int b, int C[], int c){
         for (int i = 0; i < maxL-1; i++){
                 cout << result.at(i) << endl;
         }
-        
-        
-        
 }
-
-int main() {
-        int a = 5, b = 4, c = 3;
-        // First you need to initialize all elements in A and B to NULL
-        int **A[5] = {}, *B[4] = {}, C[3] = {2, 1, 4};
-        // A is an array of pointers, and each non-NULL element points to an element of B
-        // B is an array of pointers, and each non-NULL element points to an element of C
-        chasing(A, a, B, b, C, c);
-
-        int **ansA[5] = {} ,*ansB[4] = {};
-        for (int i = 0; i < a; i++)
-                ansA[i] = NULL;
-        for (int i = 0; i < b; i++)
-                ansB[i] = NULL;
         
-        char sampleInputNum = 3; // Change number depending on the sample inputs
-        switch (sampleInputNum)
-        {
-        case 1:
-                ansA[0] = &B[0], ansA[1] = &B[2], ansB[0] = &C[2], ansB[1] = &C[1];
-                break;
-        case 2:
-                ansA[0] = &B[1];
-                break;
-        case 3:
-                ansA[2] = &B[1], ansA[3] = &B[0], ansB[1] = &C[1], ansB[2] = &C[0];
-                break;
-        default:
-                break;
-        }
-        // Check A
-        for (int i = 0; i < a; i++)
-                if (A[i] != ansA[i] )
-                cout << "A[" << i << "] " << "wrong answer" << endl;
- 
-        // Check B
-        for (int i = 0; i < b; i++)
-                if (B[i] != ansB[i] )
-                        cout << "B[" << i << "] " << "wrong answer" << endl;
-
-        return 0;
-}
+        
